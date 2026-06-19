c85caaa9ecf76090e94dc315af74bee243d64b75
nandan bali <f20240380@pilani.bits-pilani.ac.in>
2026-06-19 08:00:49 +0000

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,30 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.word = False
+        self.children: dict[str, PrefixTree] = {}
+
+    def insert(self, word: str) -> None:
+        curr = self
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = PrefixTree()
+            curr = curr.children[c]
+        curr.word = True
+
+
+    def search(self, word: str) -> bool:
+        curr = self
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.word
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
