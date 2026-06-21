083844c92c005e8e501800fb703841451628da88
nandan bali <f20240380@pilani.bits-pilani.ac.in>
2026-06-21 14:51:04 +0000

    Add: verifying-an-alien-dictionary - submission-1

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        order_map = {}
+        for index, c in enumerate(order):
+            order_map[c] = index
+        for i in range(0, len(words)-1):
+            for idx in range(0, max(len(words[i]), len(words[i+1]))):
+                if idx >= len(words[i+1]):
+                    return False
+                if idx >= len(words[i]):
+                    continue
+                if order_map[words[i][idx]] > order_map[words[i+1][idx]]:
+                    return False
+                elif order_map[words[i][idx]] < order_map[words[i+1][idx]]:
+                    break
+                else:
+                    continue
+        return True
