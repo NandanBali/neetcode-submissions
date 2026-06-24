1685f16744782ba63286a052c8275ce7cfe84b18
nandan bali <f20240380@pilani.bits-pilani.ac.in>
2026-06-24 14:18:00 +0000

    Add: accounts-merge - submission-1

---
+++ Data Structures & Algorithms/accounts-merge/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        n = len(accounts)
+        parent = list(range(n))
+        size = [1] * n
+
+        def find(a):
+            while parent[a] != a:
+                parent[a] = parent[parent[a]]
+                a = parent[a]
+            return a
+
+        def union(a, b):
+            pa, pb = find(a), find(b)
+            if pa == pb:
+                return
+            if size[pb] > size[pa]:
+                pa, pb = pb, pa
+            parent[pb] = pa
+            size[pa] += size[pb]
+
+        owner = {}                       # email -> account index
+        for i, acct in enumerate(accounts):
+            for email in acct[1:]:
+                if email in owner:
+                    union(i, owner[email])
+                else:
+                    owner[email] = i
+
+        groups = defaultdict(list)
+        for email, i in owner.items():
+            groups[find(i)].append(email)
+
+        return [[accounts[root][0]] + sorted(emails)
+                for root, emails in groups.items()]
